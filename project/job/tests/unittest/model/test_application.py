3a2a3aba8a832d27920587aac6a4d00037ca75f5
Ricardo-Jackson-Ferrari <ricjferrari@gmail.com>
2022-09-25 05:33:31 -0300

    test(app:job): update application model test

--- project/job/tests/unittest/model/test_application.py
+++ project/job/tests/unittest/model/test_application.py
@@ -1,8 +1,31 @@
 from django.test import TestCase
+from django.utils.translation import gettext_lazy as _
 from model_bakery import baker
+from parameterized import parameterized
+
+from project.job.models import Application
 
 
 class SchoolingUnittest(TestCase):
     def test_return_str(self):
-        application = baker.make('Application')
+        application = baker.make(Application)
         self.assertEqual(application.job.title, str(application))
+
+    @parameterized.expand(
+        [
+            (
+                _('%(model_name)s with this %(field_labels)s already exists.'),
+                ('schooling', 'job'),
+            ),
+            (
+                _('%(candidate)s already registered for job.'),
+                ('candidate', 'job'),
+            ),
+        ]
+    )
+    def test_unique_error_message(self, msg, unique_check):
+        application = baker.make(Application)
+        error = application.unique_error_message(
+            model_class=Application, unique_check=unique_check
+        )
+        self.assertEqual(msg, error.message)
