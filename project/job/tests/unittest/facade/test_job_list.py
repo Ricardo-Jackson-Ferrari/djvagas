34b163d77e91848cce39e08d1b555640e97094ad
Ricardo-Jackson-Ferrari <ricjferrari@gmail.com>
2022-09-26 18:16:02 -0300

    refactor(app:job): update tests

--- project/job/tests/unittest/facade/test_job_list.py
+++ project/job/tests/unittest/facade/test_job_list.py
@@ -1,11 +1,17 @@
 from django.test import TestCase
+from model_bakery import baker
 
 from project.job.facade import get_job_list
 from project.job.models import Job
 
 
 class JobListUnittest(TestCase):
-    def test_job_list_return(self):
+    def test_get_job_list_queryset_return(self):
         self.assertQuerysetEqual(
             Job.objects.filter(status=True), get_job_list()
         )
+
+    def test_get_job_list_count_return(self):
+        baker.make(Job, status=False)
+        baker.make(Job, status=True)
+        self.assertEqual(1, get_job_list().count())
