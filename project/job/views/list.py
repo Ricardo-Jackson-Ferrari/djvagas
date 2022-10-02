a1a1c89a160628be6f55db5121dd3f9a928d9a1f
Ricardo-Jackson-Ferrari <ricjferrari@gmail.com>
2022-10-02 00:54:54 -0300

    refactor(app:job): update get_queryset in JobList

--- project/job/views/list.py
+++ project/job/views/list.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import ListView
 
@@ -7,6 +8,8 @@ from project.job import facade
 class JobList(ListView):
     template_name = 'job/job_list.html'
     extra_context = {'tittle': _('job list')}
+    paginate_by = settings.PER_PAGE
 
     def get_queryset(self):
-        return facade.get_activated_authorized_job_list()
+        self.queryset = facade.get_activated_authorized_job_list()
+        return super().get_queryset()
